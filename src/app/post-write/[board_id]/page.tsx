104b75b021c96dfa2c44bb5f0316aa2da3b1e6b8
computer-note <planomenes@gmail.com>
2024-08-02 19:01:20 +0900

    update(post-write/[board_id]/page): 포스트 서식 부분을 하위 클라이언트 컴포넌트로 분리

--- src/app/post-write/[board_id]/page.tsx
+++ src/app/post-write/[board_id]/page.tsx
@@ -1,26 +1,22 @@
-import { PostFormType } from '@/types/common';
-import { FormEvent } from 'react';
+import { SupabaseServerApi } from '@/api/supabase.server.api';
+import PostWriteForm from '../_components/PostWriteForm';
 
 interface Props {
   params: { board_id: string };
 }
 
-function PostWritePage({ params: { board_id } }: Props) {
-  function handleWritePost(e: FormEvent) {}
+//Todo. 로그인하지 않은 유저가 하드내비게이션으로 해당 페이지에 접근했을 때 리다이렉트하는 로직
+async function PostWritePage({ params: { board_id } }: Props) {
+  const user = await SupabaseServerApi.getUser();
+
+  if (!user) {
+    return <p>로그인한 사용자만 게시글을 작성할 수 있습니다.</p>;
+  }
 
   return (
     <section>
-      <form onSubmit={handleWritePost}>
-        <div>
-          <label>제목</label> <input />
-        </div>
-        <div>
-          <label>이미지</label> <input />
-        </div>
-        <div>
-          <label>내용</label> <input />
-        </div>
-      </form>
+      <span>{board_id}</span>
+      <PostWriteForm board_id={+board_id} user_id={user.id} />
     </section>
   );
 }
