898efd4dbc48385c74caec27f6852711d08a3883
computer-note <planomenes@gmail.com>
2024-08-02 10:18:06 +0900

    update: Supabase DB 수정사항 반영 및 genTypes 명령어 수정
    1. Supabase DB 수정사항
      comments 테이블의 created_at 필드를 자동으로 생성하도록 변경했습니다.
    
    2. d.ts 확장자를 사용하지 않기로 한 점을 genTypes 스크립트 명령어에도 반영했습니다.

--- src/types/database.ts
+++ src/types/database.ts
@@ -44,21 +44,21 @@ export type Database = {
           created_at: string;
           id: string;
           post_id: string;
-          user_id: string | null;
+          user_id: string;
         };
         Insert: {
           content?: string;
-          created_at: string;
+          created_at?: string;
           id?: string;
           post_id: string;
-          user_id?: string | null;
+          user_id: string;
         };
         Update: {
           content?: string;
           created_at?: string;
           id?: string;
           post_id?: string;
-          user_id?: string | null;
+          user_id?: string;
         };
         Relationships: [
           {
