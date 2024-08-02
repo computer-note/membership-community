d777787ed76025fa666092de4bef6846db814423
computer-note <planomenes@gmail.com>
2024-08-02 15:37:13 +0900

    update (CommentItem): API를 사용하여 댓글 수정 UI 로직 구현

--- src/app/post-detail/_components/CommentItem.tsx
+++ src/app/post-detail/_components/CommentItem.tsx
@@ -1,4 +1,9 @@
-import { CommentType } from '@/types/common';
+'use client';
+
+import { CommentType, UserInfoType } from '@/types/common';
+import { useAuthContext } from './../../../providers/AuthContextProvider';
+import { SupabaseBrowserApi } from '@/api/supabase.browser.api';
+import { useRef } from 'react';
 
 interface Props {
   commentItem: CommentType;
@@ -8,19 +13,70 @@ function CommentItem({ commentItem }: Props) {
   const {
     content,
     created_at,
-    id,
+    id: comment_id,
     user_id,
     user_nickname,
     user_rank_name,
   } = commentItem;
 
-  return (
-    <div className='flex flex-col items-start'>
-      <span>작성일: {created_at.toDateString()}</span>
-      <span>작성자: {`${user_nickname} (${user_rank_name})`} </span>
-      <span>댓글내용: {content}</span>
+  const user = useAuthContext();
+  const commentInputRef = useRef<HTMLSpanElement | null>(null);
 
-      <button>수정삭제</button>
+  async function handleDeleteComment() {
+    //Todo. 에러처리 로직추가
+    await SupabaseBrowserApi.deleteComment(comment_id);
+
+    alert('댓글이 삭제되었습니다.');
+  }
+
+  function handleClickModify() {
+    const commentInput = commentInputRef.current!;
+
+    if (!commentInput.isContentEditable) {
+      commentInput.contentEditable = 'true';
+      commentInput.focus();
+      commentInput.addEventListener('keydown', handleModifyInput);
+    }
+  }
+
+  async function handleModifyInput(e: globalThis.KeyboardEvent) {
+    if (e.code === 'Enter') {
+      const commentInput = commentInputRef.current!;
+
+      await SupabaseBrowserApi.modifyComment({
+        comment_id,
+        content: commentInput.innerText,
+      });
+
+      commentInput.contentEditable = 'false';
+      commentInput.removeEventListener('keydown', handleModifyInput);
+
+      alert('댓글이 수정되었습니다.');
+    }
+
+    if (e.code === 'Escape') {
+      const commentInput = commentInputRef.current!;
+      commentInput.innerText = content;
+
+      commentInput.contentEditable = 'false';
+      commentInput.removeEventListener('keydown', handleModifyInput);
+    }
+  }
+
+  return (
+    <div className='flex flex-col items-start w-[80%]'>
+      <div>작성일: {created_at.toDateString()}</div>
+      <div>작성자: {`${user_nickname} (${user_rank_name})`} </div>
+      <div>
+        댓글내용: <span ref={commentInputRef}>{content}</span>
+      </div>
+
+      {user?.id === user_id ? (
+        <div>
+          <button onClick={handleClickModify}>수정</button>
+          <button onClick={handleDeleteComment}>삭제</button>
+        </div>
+      ) : null}
     </div>
   );
 }
