6ae1bb467c2d4d2cc59ef881184f7b6832d9fefe
computer-note <planomenes@gmail.com>
2024-08-02 11:58:16 +0900

    update(CommonLayout): 공통레이아웃에서 현재 로그인된 유저 정보를 AuthContextProvider를 통해 하위 컴포넌트들에게 주입

--- src/components/layout/CommonLayout.tsx
+++ src/components/layout/CommonLayout.tsx
@@ -1,20 +1,25 @@
-import { PropsWithChildren } from 'react';
 import Header from './Header';
 import Sidebar from './Sidebar';
+import { PropsWithChildren } from 'react';
+
+import AuthContextProvider from '@/providers/AuthContextProvider';
+import { SupabaseServerApi } from '@/api/supabase.server.api';
 
 /*
   CommonLayout 구현
   관리자페이지에서는 사이드바를 표시하지 않습니다.
 */
-function CommonLayout({ children }: PropsWithChildren) {
+async function CommonLayout({ children }: PropsWithChildren) {
+  const user = await SupabaseServerApi.getUser();
+
   return (
-    <>
+    <AuthContextProvider value={user}>
       <Header />
       <main className='flex gap-5'>
         <Sidebar />
         {children}
       </main>
-    </>
+    </AuthContextProvider>
   );
 }
 
