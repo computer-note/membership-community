3dd8a2ee0d2840cb6dd7e7ad56e7e20929302285
computer-note <planomenes@gmail.com>
2024-08-05 18:15:36 +0900

    update: 게시물 목록 line-height 값과 아이콘 mb 값 조정

--- src/components/BoardList.tsx
+++ src/components/BoardList.tsx
@@ -5,10 +5,10 @@ async function BoardList() {
   const boardList = await SupabaseServerApi.getBoardList();
 
   return (
-    <ul className='px-[10px] mt-6 text-[13px] leading-[21px] '>
+    <ul className='px-[10px] mt-6 text-[13px] leading-[24px] '>
       {boardList.map(board => (
         <li key={board.id} className='hover:underline'>
-          <img src='/board_prefix.png' className='inline' />
+          <img src='/board_prefix.png' className='inline mb-[1px]' />
           <Link href={`/board/${board.id}`}>{`${board.name}`}</Link>
         </li>
       ))}
