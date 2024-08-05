f7747f6bffaa7f1b5b9fd54f378250fe7b8b1dc7
computer-note <planomenes@gmail.com>
2024-08-05 19:01:42 +0900

    update: 메인 영역 위에 마진 40px 적용

--- src/components/layout/CommonLayout.tsx
+++ src/components/layout/CommonLayout.tsx
@@ -15,7 +15,7 @@ async function CommonLayout({ children }: PropsWithChildren) {
   return (
     <AuthContextProvider value={user}>
       <Header />
-      <main className='w-[1080px] mx-auto flex gap-[20px]'>
+      <main className='w-[1080px] mx-auto mt-[40px] flex gap-[20px]'>
         <Sidebar /> {/* 200px */}
         <section className='w-[860px]'>{children}</section>
       </main>
