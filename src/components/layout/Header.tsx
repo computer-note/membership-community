a6c329b65519e47a383bb8c82c4d5def911b4140
computer-note <planomenes@gmail.com>
2024-08-05 17:57:23 +0900

    update: 헤더 클릭시 메인페이지로 링크

--- src/components/layout/Header.tsx
+++ src/components/layout/Header.tsx
@@ -4,14 +4,16 @@ import Link from 'next/link';
 function Header() {
   return (
     <header className='bg-green-300 w-[1080px]  mx-auto '>
-      <div className='w-[1080px]'>
-        <Image
-          src={'/headerbanner_web_X2.png'}
-          width={1080}
-          height={130}
-          alt={'banner'}
-        />
-      </div>
+      <Link href={'/'}>
+        <div className='w-[1080px]'>
+          <Image
+            src={'/headerbanner_web_X2.png'}
+            width={1080}
+            height={130}
+            alt={'banner'}
+          />
+        </div>
+      </Link>
       <Link href={'/admin'}>Admin 페이지로</Link>
     </header>
   );
