a6e0abdb4ceb3e3ad9d3d4a896e856e017f88680
computer-note <planomenes@gmail.com>
2024-08-07 13:56:26 +0900

    update(format > convertWonFormat): 한국 숫자표시형식에 맞춰 수를 표시
    ex) 150000 => 150,000

---
+++ src/utils/format.ts
@@ -0,0 +1,20 @@
+export function extractYYYYMMDD(
+  date: Date,
+  delimiter: string = '.'
+): string {
+  const month = date.getMonth().toString().padStart(2, '0');
+
+  const day = date.getDate().toString().padStart(2, '0');
+
+  return `${date.getFullYear()}${delimiter}${month}${delimiter}${day}`;
+}
+
+export function extractHHMM(date: Date): string {
+  const minute = date.getMinutes().toString().padStart(2, '0');
+
+  return `${date.getHours()}:${minute}`;
+}
+
+export function convertWonFormat(number: number | string) {
+  return new Intl.NumberFormat('ko-KR').format(+number);
+}
