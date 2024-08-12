1073ce32e052cabac30392f878d6597e14f08de9
computer-note <planomenes@gmail.com>
2024-08-12 18:49:03 +0900

    update(mypage/page): 로그인된 유저 정보를 패치 후 API에 전달하여 해당 유저가 작성한 포스트 리스트를 패치하여 포스트 리스트 컴포넌트에 전달

--- src/app/mypage/page.tsx
+++ src/app/mypage/page.tsx
@@ -1,5 +1,21 @@
-function page() {
-  return <section>page</section>;
+import { SupabaseServerApi } from '@/api/supabase.server.api';
+import PostList from './_components/PostList';
+
+async function page() {
+  const user = await SupabaseServerApi.getUser();
+
+  const postList = await SupabaseServerApi.getPostListByUserId(
+    user?.id!
+  );
+
+  console.log('postList ↓');
+  console.dir(postList);
+
+  return (
+    <section>
+      <PostList postList={postList} />
+    </section>
+  );
 }
 
 export default page;
