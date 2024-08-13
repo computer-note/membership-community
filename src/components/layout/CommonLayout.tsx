5d58bbf93c7e2c1609be3e7015ea9c2f37fb2986
computer-note <planomenes@gmail.com>
2024-08-13 21:08:47 +0900

    update(CommonLayout): Tanstack Query Provider 적용

--- src/components/layout/CommonLayout.tsx
+++ src/components/layout/CommonLayout.tsx
@@ -4,21 +4,20 @@ import { PropsWithChildren } from 'react';
 
 import AuthContextProvider from '@/providers/AuthContextProvider';
 import { SupabaseServerApi } from '@/api/supabase.server.api';
+import QueryProvider from '@/providers/QueryProvider';
 
-/*
-  CommonLayout 구현
-  관리자페이지에서는 사이드바를 표시하지 않습니다.
-*/
 async function CommonLayout({ children }: PropsWithChildren) {
   const user = await SupabaseServerApi.getUser();
 
   return (
     <AuthContextProvider value={user}>
-      <Header />
-      <main className='w-[1080px] mx-auto mt-[40px] flex gap-[20px]'>
-        <Sidebar /> {/* 200px */}
-        <section className='w-[860px]'>{children}</section>
-      </main>
+      <QueryProvider>
+        <Header />
+        <main className='w-[1080px] mx-auto mt-[40px] flex gap-[20px]'>
+          <Sidebar /> {/* 200px */}
+          <section className='w-[860px]'>{children}</section>
+        </main>
+      </QueryProvider>
     </AuthContextProvider>
   );
 }
