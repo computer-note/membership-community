3b18cebb3d94fc6dbfaed6b229111dfa826906bf
computer-note <planomenes@gmail.com>
2024-08-13 21:06:03 +0900

    refactor: 패치에러 UI 제거

--- src/app/post-detail/[id]/page.tsx
+++ src/app/post-detail/[id]/page.tsx
@@ -10,10 +10,6 @@ async function PostDetailPage({ params }: Props) {
   const postId = params.id;
   const postDetail = await SupabaseServerApi.getPostDetail(postId);
 
-  if (!postDetail) {
-    return <>Error Fetching Post Detail</>;
-  }
-
   return (
     <div className='flex flex-col border border-[#ebecef] rounded-[6px] p-[29px] mr-[5px]'>
       <PostDetail postDetail={postDetail} />
