a107a987d3601e3e7ebc693cca005ec4fb5831f2
computer-note <planomenes@gmail.com>
2024-08-13 21:42:03 +0900

    update(PostButtons): 전체선택 버튼 로직 구현

--- src/app/mypage/_components/PostButtons.tsx
+++ src/app/mypage/_components/PostButtons.tsx
@@ -1,14 +1,38 @@
+'use client';
+
 import Link from 'next/link';
 
 function PostButtons() {
+  function handleDeleteButtonClick() {}
+
+  function handleSelectAllCheckBoxChange(
+    e: React.ChangeEvent<HTMLInputElement>
+  ) {
+    const checkboxList = document.querySelectorAll<HTMLInputElement>(
+      "input[type='checkbox']"
+    );
+    const changedValue = e.target.checked;
+
+    checkboxList.forEach(
+      checkbox => (checkbox.checked = changedValue)
+    );
+  }
+
   return (
     <section className='pl-[4px] mt-[10px] h-[34px] text-[12px] '>
       <div className='float-left'>
-        <input type='checkbox' /> 전체선택
+        <input
+          onChange={handleSelectAllCheckBoxChange}
+          type='checkbox'
+        />
+        <span className='ml-[6px]'>전체선택</span>
       </div>
 
       <div className='float-right'>
-        <button className='w-[54px] h-[34px] rounded-[6px] bg-[#eff0f2] font-[700] '>
+        <button
+          onClick={handleDeleteButtonClick}
+          className='w-[54px] h-[34px] rounded-[6px] bg-[#eff0f2] font-[700] '
+        >
           삭제
         </button>
         <Link
