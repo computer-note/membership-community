9cac4cb94b9d4aac00db4c6ee04114a1f601bb6f
computer-note <planomenes@gmail.com>
2024-08-14 22:43:42 +0900

    update(TextInput): TextInput 컴포넌트에 defaultValue 프롭스 추가

--- src/app/post-write/_components/TextInput.tsx
+++ src/app/post-write/_components/TextInput.tsx
@@ -1,14 +1,16 @@
 interface Props {
   placeholder: string;
   name: string;
+  defaultValue: string | undefined;
 }
 
-function TextInput({ placeholder, name }: Props) {
+function TextInput({ placeholder, name, defaultValue }: Props) {
   return (
     <input
       className='w-[100%] h-[50px] rounded-[12px] bg-[#f5f6f8] px-[16px]'
       placeholder={placeholder}
       name={name}
+      defaultValue={defaultValue}
     />
   );
 }
