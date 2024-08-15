cb4b631992fe2f20e3eef1ee7475c072e3ae5293
computer-note <planomenes@gmail.com>
2024-08-16 05:37:47 +0900

    chore(TextInput): 프롭스 이름 name에서 htmlName으로 변경

--- src/app/post-write/_components/TextInput.tsx
+++ src/app/post-write/_components/TextInput.tsx
@@ -1,15 +1,15 @@
 interface Props {
   placeholder: string;
-  name: string;
+  htmlName: string;
   defaultValue: string | undefined;
 }
 
-function TextInput({ placeholder, name, defaultValue }: Props) {
+function TextInput({ placeholder, htmlName, defaultValue }: Props) {
   return (
     <input
       className='w-[100%] h-[50px] rounded-[12px] bg-[#f5f6f8] px-[16px]'
       placeholder={placeholder}
-      name={name}
+      name={htmlName}
       defaultValue={defaultValue}
     />
   );
