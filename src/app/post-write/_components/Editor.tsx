752d777630768ce1cd3db2bb515fe812203e4b8d
computer-note <planomenes@gmail.com>
2024-08-16 05:36:39 +0900

    add(Editor): React-Quill 에디터 컴포넌트 추가

---
+++ src/app/post-write/_components/Editor.tsx
@@ -0,0 +1,28 @@
+'use client';
+
+import { Dispatch, SetStateAction } from 'react';
+import ReactQuill from 'react-quill';
+import 'react-quill/dist/quill.snow.css';
+
+interface Props {
+  setEditorContent: Dispatch<SetStateAction<string>>;
+}
+
+function Editor({ setEditorContent }: Props) {
+  const modules = {
+    toolbar: {
+      container: [
+        [{ size: ['small', false, 'large', 'huge'] }],
+        ['bold', 'italic', 'underline', 'strike'],
+      ],
+    },
+  };
+
+  function handleOnChange(value: string) {
+    setEditorContent(value);
+  }
+
+  return <ReactQuill modules={modules} onChange={handleOnChange} />;
+}
+
+export default Editor;
