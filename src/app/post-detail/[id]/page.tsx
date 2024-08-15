7849c17c43d0911170531af3361d7bc7bc978b9f
computer-note <planomenes@gmail.com>
2024-08-16 07:51:20 +0900

    update(post-detail): 페이지 하단 버튼그룹 UI 추가

--- src/app/post-detail/[id]/page.tsx
+++ src/app/post-detail/[id]/page.tsx
@@ -1,6 +1,8 @@
 import { SupabaseServerApi } from '@/api/supabase.server.api';
+
 import PostDetail from '../_components/PostDetail';
 import CommentPanel from '../_components/CommentPanel';
+import BottomButtons from '../_components/BottomButtons';
 
 interface Props {
   params: { id: string };
@@ -10,11 +12,21 @@ async function PostDetailPage({ params }: Props) {
   const postId = params.id;
   const postDetail = await SupabaseServerApi.getPostDetail(postId);
 
+  //Todo. postId에 해당하는 포스트가 없을 시 처리
+
   return (
-    <div className='flex flex-col border border-[#ebecef] rounded-[6px] p-[29px] mr-[5px]'>
-      <PostDetail postDetail={postDetail!} />
-      <CommentPanel postId={postId} />
-    </div>
+    <>
+      <div className='flex flex-col border border-[#ebecef] rounded-[6px] p-[29px] mr-[5px]'>
+        <PostDetail postDetail={postDetail!} />
+        <CommentPanel postId={postId} />
+      </div>
+
+      <BottomButtons
+        className='mt-[13px]'
+        postId={postId}
+        boardId={postDetail?.board_id}
+      />
+    </>
   );
 }
 
